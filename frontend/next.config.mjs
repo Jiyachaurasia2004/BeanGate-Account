c7d012dec7dacb288b672549785b997e7fa694fa
Jiya Chaurasia <jiyachaurasia25@gmail.com>
2025-11-22 10:36:46 +0530

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,14 +1,7 @@
-import { fileURLToPath } from 'url';
-import path from 'path';
-
-// __dirname equivalent for ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   turbopack: {
-    root: __dirname,
+    root: ".",
   },
 };
 
